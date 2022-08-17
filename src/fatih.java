e93952096cf47d3c0b4a14babca6f5a342f4ae68
fatihhhh <fatihisiteknik42@gmail.com>
2022-08-17 16:21:33 +0300

    Ahmet branch ikinci değişiklik

--- src/fatih.java
+++ src/fatih.java
@@ -1,5 +1,10 @@
 public class fatih {
     public static void main(String[] args) {
+
         System.out.println("ddd");
+        System.out.println("ikinci değişiişklik");
+
+
+        System.out.println("fatih branchınde ortak clasında değişiklik yaptım");
     }
 }
